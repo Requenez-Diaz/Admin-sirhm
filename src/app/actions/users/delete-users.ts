19a20a8906964437f6a6b05d9ab6f0b29fd40e6e
Requenez <95236561+Requenez-Diaz@users.noreply.github.com>
2024-10-23 11:54:26 -0600

    [fix]: moving components folder to one where each of the admin panel routes are nested, and correcting errors in delete_roles and delete users

--- src/app/actions/users/delete-users.ts
+++ src/app/actions/users/delete-users.ts
@@ -6,13 +6,6 @@ export default async function deleteUsers(formData: FormData) {
   const userId = formData.get("userId")?.toString();
   console.log("Form data of the users: ", formData);
 
-  const formDataArray = Array.from(formData.entries());
-  for (const [key, value] of formDataArray) {
-    console.log(key, value);
-  }
-
-  console.log("User ID: ", userId);
-
   if (!userId) {
     console.error("No user ID provided");
     return;
@@ -21,8 +14,6 @@ export default async function deleteUsers(formData: FormData) {
   try {
     const parsedUserId = parseInt(userId);
 
-    console.log("Parsed User ID: ", parsedUserId);
-
     await prisma.user.delete({
       where: {
         id: parsedUserId,
@@ -30,23 +21,6 @@ export default async function deleteUsers(formData: FormData) {
     });
 
     console.log("User deleted");
-    const users = await prisma.user.findMany({
-      orderBy: {
-        username: "asc",
-      },
-    });
-
-    for (let user = 0; user < users.length; user++) {
-      const currentNumber = user + 1;
-      await prisma.user.update({
-        where: {
-          id: users[user].id,
-        },
-        data: {
-          username: currentNumber.toString(),
-        },
-      });
-    }
     revalidatePath("/users");
   } catch (error) {
     console.error("Error deleting user: ", error);
