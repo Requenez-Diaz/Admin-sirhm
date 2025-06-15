2f149ff69e9dc87d087daaa1d237bd068a106be0
Elliam Sánchez <elliamsanchez510@gmail.com>
2025-06-14 18:01:38 -0600

    feat(dashboard): implement room type summary table

--- src/app/(routes)/dashboard/reports/page.tsx
+++ src/app/(routes)/dashboard/reports/page.tsx
@@ -1,13 +1,17 @@
+import { getReservations } from '@/app/actions/reservation'
+import RoomTypeTable from './RoomTypeTable'
 import ExcelReportGenerator from './GenerateExcelReport'
 import ReportsPage from './ReportDashboard'
 
 export default async function DashboardPage() {
+  const reservations = await getReservations()
 
   return (
     <div className="space-y-6">
       <h1 className="text-2xl font-bold">Reportes de Reservas</h1>
       <ExcelReportGenerator />
       <ReportsPage />
+      <RoomTypeTable reservations={reservations} />
     </div>
   )
 }
