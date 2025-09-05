d19a222bab680cbb5c239d8aee66e1f13f677410
Elliam Sánchez <elliamsanchez510@gmail.com>
2025-09-05 11:06:22 -0600

    feat(dashboard): fetch real data from database

--- src/app/(routes)/dashboard/home/page.tsx
+++ src/app/(routes)/dashboard/home/page.tsx
@@ -1,65 +1,48 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { UsersRound, Hotel, Calendar, ShoppingCart } from "lucide-react";
+import { DashboardCard } from "./DashboardCard";
+import { getReservations } from "@/app/actions/reservation";
+import { getServices } from "@/app/actions/services";
+import { getBedrooms } from "@/app/actions/bedrooms";
+import { findManyUsers } from "@/app/actions/users";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+
+  const reservations = await getReservations();
+  const services = await getServices();
+  const bedrooms = await getBedrooms();
+  const user = await findManyUsers();
   return (
     <div>
-      <h1 className='text-2xl font-bold mb-6'>Panel de Control</h1>
+      <h1 className="text-2xl font-bold mb-6">Panel de Control</h1>
 
-      <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-4'>
-        <Card>
-          <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-            <CardTitle className='text-sm font-medium'>
-              Total Usuarios
-            </CardTitle>
-            <UsersRound className='h-4 w-4 text-muted-foreground' />
-          </CardHeader>
-          <CardContent>
-            <div className='text-2xl font-bold'>1</div>
-            <p className='text-xs text-muted-foreground'>
-              +0% desde el último mes
-            </p>
-          </CardContent>
-        </Card>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        <DashboardCard
+          title="Total Usuarios"
+          value={user.length}
+          description="+0% desde el último mes"
+          icon={UsersRound}
+        />
 
-        <Card>
-          <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-            <CardTitle className='text-sm font-medium'>Habitaciones</CardTitle>
-            <Hotel className='h-4 w-4 text-muted-foreground' />
-          </CardHeader>
-          <CardContent>
-            <div className='text-2xl font-bold'>0</div>
-            <p className='text-xs text-muted-foreground'>
-              +0% desde el último mes
-            </p>
-          </CardContent>
-        </Card>
+        <DashboardCard
+          title="Habitaciones"
+          value={bedrooms.length}
+          description="+0% desde el último mes"
+          icon={Hotel}
+        />
 
-        <Card>
-          <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-            <CardTitle className='text-sm font-medium'>Reservaciones</CardTitle>
-            <Calendar className='h-4 w-4 text-muted-foreground' />
-          </CardHeader>
-          <CardContent>
-            <div className='text-2xl font-bold'>0</div>
-            <p className='text-xs text-muted-foreground'>
-              +0% desde el último mes
-            </p>
-          </CardContent>
-        </Card>
+        <DashboardCard
+          title="Reservaciones"
+          value={reservations.length}
+          description="+0% desde el último mes"
+          icon={Calendar}
+        />
 
-        <Card>
-          <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-            <CardTitle className='text-sm font-medium'>Servicios</CardTitle>
-            <ShoppingCart className='h-4 w-4 text-muted-foreground' />
-          </CardHeader>
-          <CardContent>
-            <div className='text-2xl font-bold'>0</div>
-            <p className='text-xs text-muted-foreground'>
-              +0% desde el último mes
-            </p>
-          </CardContent>
-        </Card>
+        <DashboardCard
+          title="Servicios"
+          value={services.length}
+          description="+0% desde el último mes"
+          icon={ShoppingCart}
+        />
       </div>
     </div>
   );
