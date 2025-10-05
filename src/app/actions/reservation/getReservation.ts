b5b625eaae8f5e9a2f1a9c245cfe3f2bf6387ceb
Requenez <95236561+Requenez-Diaz@users.noreply.github.com>
2025-10-05 15:31:51 -0600

    [fix]: getting the data to offerts in the reservations

--- src/app/actions/reservation/getReservation.ts
+++ src/app/actions/reservation/getReservation.ts
@@ -3,15 +3,28 @@
 import prisma from "@/lib/db";
 
 export const getReservations = async () => {
-    try {
-        const reservations = await prisma.reservation.findMany({
-            orderBy: {
-                createdAt: 'desc',
-            },
-        });
-        return reservations;
-    } catch (error) {
-        console.error("Error al obtener las reservas", error);
-        return [];
-    }
+  try {
+    const reservations = await prisma.reservation.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        Promotions: {
+          select: {
+            codePromotions: true,
+          },
+        },
+      },
+    });
+
+    const formattedReservations = reservations.map((reservation) => ({
+      ...reservation,
+      offerts: reservation.Promotions?.codePromotions || null,
+    }));
+
+    return formattedReservations;
+  } catch (error) {
+    console.error("Error al obtener las reservas", error);
+    return [];
+  }
 };
