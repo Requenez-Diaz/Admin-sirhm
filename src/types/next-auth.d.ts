c09cd53c42b01a1112d1528a236b93a7e204e041
Requenez-Diaz <alfredorequenez57libra@gmail.com>
2026-02-27 15:21:32 -0600

    [fix error for login]

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,20 +1,22 @@
-import { Role } from "@prisma/client";
 import { DefaultSession } from "next-auth";
+import { JWT as DefaultJWT } from "next-auth/jwt";
 
 declare module "next-auth" {
   interface Session {
     user: {
-      role: Role;
-    } & DefaultSession["admin"];
+      id: string;
+      role: string;
+    } & DefaultSession["user"];
   }
 
-  interface User extends DefaultUser {
+  interface User {
     role?: string;
   }
+}
 
-  declare module "next-auth/jwt" {
-    interface JWT {
-      role: string;
-    }
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    id: string;
+    role: string;
   }
 }
